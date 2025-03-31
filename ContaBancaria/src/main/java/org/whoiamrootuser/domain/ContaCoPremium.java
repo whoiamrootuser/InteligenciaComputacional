aa7738b83d9c958458e01a674337734831fc4272
Joao Henrique <jhenriquejrc@gmail.com>
2025-03-31 17:17:02 -0300

    feat: aplicacao Conta Bancaria exemplo heranca

---
+++ ContaBancaria/src/main/java/org/whoiamrootuser/domain/ContaCoPremium.java
@@ -0,0 +1,23 @@
+package org.whoiamrootuser.domain;
+
+public class ContaCoPremium extends ContaCo {
+    final private double cashBackPercentual;
+    public ContaCoPremium(String titular, double saldo, double limiteChequeEspecial, double cashBackPercentage) {
+        super(titular, saldo, limiteChequeEspecial);
+        this.cashBackPercentual = cashBackPercentage;
+    }
+
+    @Override
+    public boolean sacar(double valor){
+        super.sacar(valor);
+        saldo += valor * (cashBackPercentual/100);
+        return true;
+    }
+
+    public String exibeBeneficioPremium() {
+        String formattedPercentual = String.format("%.2f%%", cashBackPercentual);
+        String outputMessage = "Conta Corrente Premium com cashback de " + formattedPercentual + " a cada saque." ;
+        System.out.println(outputMessage);
+        return outputMessage;
+    }
+}
